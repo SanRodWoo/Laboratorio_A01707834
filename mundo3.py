5bde0c4271bb91aa7e2d5e1202c82d9a34cc2fe1
SanRodWoo <125622137+SanRodWoo@users.noreply.github.com>
2023-11-15 19:27:02 -0600

    Add files via upload

---
+++ mundo3.py
@@ -0,0 +1,13 @@
+import datetime 
+import time 
+import pandas as pd 
+import streamlit as st
+from PIL import Image 
+
+"""Generación de la webapp con streamlit"""
+st.title("Título: Analítica de Datos")
+st.header("Este es un header")
+st.subheader("Este es un subheader")
+
+"""Definir un Texto
+st.text("Texto: Hola Streamlit)"""
