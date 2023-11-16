7c0bac4105c4eb683d7cf4fe2fe6663284728cc0
SanRodWoo <125622137+SanRodWoo@users.noreply.github.com>
2023-11-15 19:35:07 -0600

    mundo3_1.py

--- mundo3.py
+++ mundo3.py
@@ -9,5 +9,18 @@ st.title("Título: Analítica de Datos")
 st.header("Este es un header")
 st.subheader("Este es un subheader")
 
-"""Definir un Texto
-st.text("Texto: Hola Streamlit)"""
+#Definir un Texto
+st.text("Texto: Hola Streamlit")
+
+#Definición de un markdown
+st.markdown("# Este es un markdown h1 \n ##Este es un h2 \n ###Este es un h3")
+st.header("Colores de texto y mensajes de error")
+st.success("Succesful")
+st.info("Información!")
+st.warning("Warning")
+st.error("Error")
+st.exception("NameError('no está definido')")
+st.header("Obtener información de ayuda de Python")
+st.help(range)
+st.header("Widgets:")
+st.subheader("Checkbox")
